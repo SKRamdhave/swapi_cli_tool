51294aa64354361267e6e81963c418e51bb18d20
SKRamdhave <suhasiramdhave@gmail.com>
2022-12-05 09:02:48 +0530

    First Commit to GIT

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+r = requests.get("https://swapi.dev/api")
+
+import pdb; pdb.set_trace()              ## to add debugger
+
+print("statement 1")
+print("statement 2")
+print("statement 3")
